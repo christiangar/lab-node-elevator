94df8ee701719d99b3fc3db5d93fc8ce21b06d09
christiangar <32417561+christiangar@users.noreply.github.com>
2017-12-15 17:39:00 +0100

    feat: Pending last Iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,53 @@
+const Person = require('./person.js');
+const SECOND = 1000;
+
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor      = 0,
+    this.MAXFLOOR   = 10,
+    this.requests   = [],
+    this.direction  = "up",
+    this.limitTop   = 6,
+    this.limitDown   = -1,
+    this.startInterval,
+    this.waitingList = []
+    this.passengers = []
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.startInterval = setInterval(() => this.update(), SECOND)
+  }
+
+  stop() {
+    clearInterval(startInterval)
+  }
+
+  update() {this.log();}
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp() {
+    if(this.floor < this.limitTop){
+      this.floor ++;
+      this.direction = 'up';
+      if (this.request.includes(this.floor))
+    }
+  }
+
+  floorDown() {
+    if(this.floor > this.limitDown) {
+      this.floor --;
+      this.direction = 'down';
+    }
+  }
+  call(person, originFloor) {
+    this.waitingList.push(person)
+    this.requests.push(originFloor)
+   }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator = new Elevator()
+const person1 = new Person("Cristian", 3, 5)
+
+
+elevator.floorDown()
+elevator.floorDown()
+elevator.floorDown()
+elevator.floorDown()
+elevator.floorDown()
+
+elevator.start()
